3fd72d693331e04df8ca7f032b44616666c72787
akw09 <44108566+akw09@users.noreply.github.com>
2021-07-21 14:07:58 +0700

    changing search

--- src/component/Search/index.js
+++ src/component/Search/index.js
@@ -15,52 +15,50 @@ class Playlist extends Component {
           .then((response) => setResult(response.tracks));
       };
     }
-    render(){
-        return(
-            <>
-            <h1 style={{ textAlign: "center" }}>Playlist Search</h1>
-            <div className="row-center">
-              <input
-                value={search}
-                onChange={(e) => {
-                  setSearch(e.target.value);
-                }}
-                className="input-text"
-                type="text"
-                name="search"
-              />
-              <button
-                onClick={() => getSearchResult()}
-                className="search-button"
-                type="button"
-              >
-                Search
-              </button>
-            </div>
-            {result ? (
-              <>
-                <h3 style={{ textAlign: "center" }}>
-                  Showing {result.items.length} tracks
-                </h3>
-                {result.items.map((track, i) => {
-                  return (
-                    <TrackComponent
-                      key={i}
-                      height={track.album.images[0].height}
-                      imgUrl={track.album.images[0].url}
-                      title={track.name}
-                      artistName={track.artists[0].name}
-                      albumName={track.album.name}
-                    />
-                  );
-                })}
-              </>
-            ) : (
-              <h3 style={{ textAlign: "center" }}>No Tracks Show</h3>
-            )}
+    render()
+    {return(
+        <>
+        <h1 style={{ textAlign: "center" }}>Playlist Search</h1>
+        <div className="row-center">
+          <input
+            value={search}
+            onChange={(e) => {
+              setSearch(e.target.value);
+            }}
+            className="input-text"
+            type="text"
+            name="search"
+          />
+          <button
+            onClick={() => getSearchResult()}
+            className="search-button"
+            type="button"
+          >
+            Search
+          </button>
+        </div>
+        {result ? (
+          <>
+            <h3 style={{ textAlign: "center" }}>
+              Showing {result.items.length} tracks
+            </h3>
+            {result.items.map((track, i) => {
+              return (
+                <TrackComponent
+                  key={i}
+                  height={track.album.images[0].height}
+                  imgUrl={track.album.images[0].url}
+                  title={track.name}
+                  artistName={track.artists[0].name}
+                  albumName={track.album.name}
+                />
+              );
+            })}
           </>
-        )
-    }
-
+        ) : (
+          <h3 style={{ textAlign: "center" }}>No Tracks Show</h3>
+        )}
+      </>
+    )}
 
 export default Playlist;
