8fa22b80f669cf1573b3b137fd908028fb3f8612
akw09 <44108566+akw09@users.noreply.github.com>
2021-07-21 14:03:44 +0700

    changing to class component playlist

--- src/component/Search/index.js
+++ src/component/Search/index.js
@@ -1,61 +1,66 @@
-import React, { useState } from "react";
+import React, { Component, useState } from "react";
 import TrackComponent from "../TrackComponent";
 
-function Playlist(props) {
-  const [result, setResult] = useState(null);
-  const [search, setSearch] = useState("");
-
-  const getSearchResult = () => {
-    fetch(`https://api.spotify.com/v1/search?q=${search}&type=track`, {
-      headers: { Authorization: "Bearer " + props.params.access_token },
-    })
-      .then((response) => response.json())
-      .then((response) => setResult(response.tracks));
-  };
-
-  return (
-    <>
-      <h1 style={{ textAlign: "center" }}>Playlist Search</h1>
-      <div className="row-center">
-        <input
-          value={search}
-          onChange={(e) => {
-            setSearch(e.target.value);
-          }}
-          className="input-text"
-          type="text"
-          name="search"
-        />
-        <button
-          onClick={() => getSearchResult()}
-          className="search-button"
-          type="button"
-        >
-          Search
-        </button>
-      </div>
-      {result ? (
-        <>
-          <h3 style={{ textAlign: "center" }}>
-            Showing {result.items.length} tracks
-          </h3>
-          {result.items.map((track, i) => {
-            return (
-              <TrackComponent
-                key={i}
-                height={track.album.images[0].height}
-                imgUrl={track.album.images[0].url}
-                title={track.name}
-                artistName={track.artists[0].name}
-                albumName={track.album.name}
+class Playlist extends Component {
+   state={
+       result = null,
+       search = ""
+   }
+   
+    getSearchResult = () => {
+        fetch(`https://api.spotify.com/v1/search?q=${search}&type=track`, {
+          headers: { Authorization: "Bearer " + props.params.access_token },
+        })
+          .then((response) => response.json())
+          .then((response) => setResult(response.tracks));
+      };
+    }
+    render(){
+        return(
+            <>
+            <h1 style={{ textAlign: "center" }}>Playlist Search</h1>
+            <div className="row-center">
+              <input
+                value={search}
+                onChange={(e) => {
+                  setSearch(e.target.value);
+                }}
+                className="input-text"
+                type="text"
+                name="search"
               />
-            );
-          })}
-        </>
-      ) : (
-        <h3 style={{ textAlign: "center" }}>No Tracks Show</h3>
-      )}
-    </>
-  );
-}
+              <button
+                onClick={() => getSearchResult()}
+                className="search-button"
+                type="button"
+              >
+                Search
+              </button>
+            </div>
+            {result ? (
+              <>
+                <h3 style={{ textAlign: "center" }}>
+                  Showing {result.items.length} tracks
+                </h3>
+                {result.items.map((track, i) => {
+                  return (
+                    <TrackComponent
+                      key={i}
+                      height={track.album.images[0].height}
+                      imgUrl={track.album.images[0].url}
+                      title={track.name}
+                      artistName={track.artists[0].name}
+                      albumName={track.album.name}
+                    />
+                  );
+                })}
+              </>
+            ) : (
+              <h3 style={{ textAlign: "center" }}>No Tracks Show</h3>
+            )}
+          </>
+        )
+    }
+
+
 export default Playlist;
