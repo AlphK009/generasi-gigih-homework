64907078780a627c03d444a149afcdb78ce73356
Alfa Alfa <alfanokurniawan@gmail.com>
2021-08-16 21:57:53 +0700

    fixing search button

--- src/Pages/search.js
+++ src/Pages/search.js
@@ -7,80 +7,87 @@ const REDIRECT_URL = "http://localhost:3000";
 const SCOPES = "playlist-modify-private";
 
 const Search = () => {
-    const [authInfo, setAuthInfo] = useState({ isAuth: false })
-    const [tracks, setTracks] = useState("")
-    const [search, setSearch] = useState("");
-    const getReturnedParams = (hash) => {
-        const stringAfterHashtag = hash.substring(1);
-        const paramsUrl = stringAfterHashtag.split("&");
-        const paramsSplit = paramsUrl.reduce((acc, currentValue) => {
-            const [key, value] = currentValue.split("=");
-            acc[key] = value;
-            return acc;
-        }, {});
-        return paramsSplit;
-    };
+  const [authInfo, setAuthInfo] = useState({ isAuth: false });
+  const [tracks, setTracks] = useState("");
+  const [search, setSearch] = useState("");
+  const getReturnedParams = (hash) => {
+    const stringAfterHashtag = hash.substring(1);
+    const paramsUrl = stringAfterHashtag.split("&");
+    const paramsSplit = paramsUrl.reduce((acc, currentValue) => {
+      const [key, value] = currentValue.split("=");
+      acc[key] = value;
+      return acc;
+    }, {});
+    return paramsSplit;
+  };
 
-    const handleSearch = async query => {
-        const SEARCH_API_ENDPOINT = `https://api.spotify.com/v1/search`
-        const SEARCH_TYPE = `track`
-        const res = await fetch(`${SEARCH_API_ENDPOINT}?q=${query}&type=${SEARCH_TYPE}&limit=12`, {
-            method: 'GET',
-            headers: {
-                'Authorization': 'Bearer ' + authInfo.access_token
-            }
-        }).then(res => res.json())
-        console.log(res.tracks.items)
-        setTracks(res.tracks.items)
+  const handleSearch = async (query) => {
+    const SEARCH_API_ENDPOINT = `https://api.spotify.com/v1/search`;
+    const SEARCH_TYPE = `track`;
+    const res = await fetch(
+      `${SEARCH_API_ENDPOINT}?q=${query}&type=${SEARCH_TYPE}&limit=12`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: "Bearer " + authInfo.access_token,
+        },
+      }
+    ).then((res) => res.json());
+    console.log(res.tracks.items);
+    setTracks(res.tracks.items);
+  };
+  const handleLogin = () => {
+    window.location = `${SPOTIFY_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL}&scope=${SCOPES}&response_type=token&show_dialog=true`;
+  };
+
+  useEffect(() => {
+    if (window.location.hash) {
+      const newAuthInfo = {
+        isAuth: true,
+        ...getReturnedParams(window.location.hash),
+      };
+      setAuthInfo(newAuthInfo);
     }
-    const handleLogin = () => {
-        window.location = `${SPOTIFY_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL}&scope=${SCOPES}&response_type=token&show_dialog=true`;
-    };
+  }, []);
 
-    useEffect(() => {
-        if (window.location.hash) {
-            const newAuthInfo = { isAuth: true, ...getReturnedParams(window.location.hash) }
-            setAuthInfo(newAuthInfo);
-        }
-    }, [])
+  return authInfo.isAuth ? (
+    <div>
+      <input
+        value={search}
+        onChange={(e) => {
+          setSearch(e.target.value);
+        }}
+        className="input-text"
+        type="text"
+        name="search"
+      />
+      <button
+        onClick={() => handleSearch(search)}
+        className="search-button"
+        type="button"
+      >
+        Search
+      </button>
 
-    return authInfo.isAuth ? (
-        <div>
-            <input
-                value={search}
-                onChange={(e) => {
-                    setSearch(e.target.value);
-                }}
-                className="input-text"
-                type="text"
-                name="search" />
-            <button
-                onClick={()=> handleSearch(search)}
-                className="search-button"
-                type="button">
-                Search
-            </button>
-
-            {tracks.map((track) => {
-                return (
-                    <TrackComponent 
-                    key={track.id}
-                    url={track.album.images[0].url} 
-                    songtitle={track.name}
-                    artistsName={track.artists[0].name}
-                    albumName={track.album.name}
-                    buttonName="select"
-                    />
-                )
-
-            })
-            }
-        </div>
-    ) : (
-        <div >
-            <button onClick={handleLogin}>Login</button>
-        </div>
-    )
-}
+      {tracks &&
+        tracks.map((track) => {
+          return (
+            <TrackComponent
+              key={track.id}
+              url={track.album.images[0].url}
+              songtitle={track.name}
+              artistsName={track.artists[0].name}
+              albumName={track.album.name}
+              buttonName="select"
+            />
+          );
+        })}
+    </div>
+  ) : (
+    <div>
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+};
 
 export default Search;
